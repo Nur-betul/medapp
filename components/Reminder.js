6ef0ae7f8eb0fbb45265e4d9429faa2660d6262c
justicepelteir <justice.douglas@hey.com>
2022-09-11 15:58:32 -0700

    create Reminder.js

---
+++ components/Reminder.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Reminder = () => {
+    return (
+        <View>
+            <Text>I am a reminder.</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Reminder;
