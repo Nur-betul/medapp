2095d4d71f91e95ea9086b6aa5b88fb3646682e8
justicepelteir <justice.douglas@hey.com>
2022-09-11 13:01:03 -0700

    add create reminder button

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color: 'purple', fontsize: 18}}>
+        To create a reminder, just press the button below!
+      </Text>
+      {/* <StatusBar style="auto" /> */}
+
+      <TouchableOpacity
+        onPress={() => alert('Hello world!')}
+        style={{backgroundColor: 'blue' }}>
+          <Text style={{ fontSize: 20, color: '#fff'}}>Create reminder</Text>
+      </TouchableOpacity>
     </View>
   );
 }
